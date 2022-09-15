4779027fe3c4b2ffa34e9c958e49211b7f9eeaf1
David Sackstein <davids@codeprecise.com>
2022-09-15 10:51:40 -0600

    No need to try catch in the ctor.

--- clean-it/Movie.h
+++ clean-it/Movie.h
@@ -11,16 +11,19 @@ namespace ci {
 	class Movie
 	{
 	public:
-		const std::chrono::seconds MaxDuration = 120s;
+		constexpr inline static std::chrono::seconds MaxDuration = 120s;
 
 		std::string Name;
 		std::chrono::seconds Duration;
 
 		static expected<Movie> create(
 			const std::string& name, int seconds) noexcept {
-			return expect([&] {
-				return Movie(name, std::chrono::seconds(seconds));
-			});
+
+			if (const auto duration = std::chrono::seconds(seconds); 
+				duration > MaxDuration || duration < 1s) {
+				return unexpected ("invalid duration");
+			}
+			return Movie(name, std::chrono::seconds(seconds));
 		}
 
 	private:
@@ -28,10 +31,6 @@ namespace ci {
 		Movie(std::string name, std::chrono::seconds seconds) :
 			Name(std::move(name)),
 			Duration(seconds) {
-
-			if (Duration > MaxDuration || Duration < 1s) {
-				throw std::runtime_error("invalid duration");
-			}
 		}
 	};
 }
