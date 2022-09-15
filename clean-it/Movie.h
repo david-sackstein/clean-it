74835ef2d2d14efba3d84ef845f2e5c1c4d2f042
David Sackstein <davids@codeprecise.com>
2022-09-15 11:30:03 -0600

    Added presentation

--- clean-it/Movie.h
+++ clean-it/Movie.h
@@ -31,6 +31,8 @@ namespace ci {
 		// The default ctor is deleted.
 		// All other special functions are defaulted and meet the invariant if their argument does,
 		// so they do not need to be redefined.
+		// Moreover, the cpp core guideline https://github.com/isocpp/CppCoreGuidelines/blob/master/CppCoreGuidelines.md#c21-if-you-define-or-delete-any-copy-move-or-destructor-function-define-or-delete-them-all
+		// does not apply here.
 		Movie(std::string name, std::chrono::seconds seconds) :
 			Name(std::move(name)),
 			Duration(seconds) {
